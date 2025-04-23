ad294fc66cf37178bfcec45c897ec2489aab2a95
Mostafa Abdallah <tofa201714@gmail.com>
2025-04-23 19:29:16 +0200

    version 3.1

--- app/client.py
+++ app/client.py
@@ -1,22 +1,29 @@
-from fastapi import WebSocket, WebSocketDisconnect
+# client.py
+
+import asyncio
 from starlette import status
+from fastapi import WebSocket, WebSocketDisconnect
 from app.shared_tools import server_ws, subscribed_clients, clients, get_clients_count
 
 async def websocket_client(websocket: WebSocket, user_id: str):
-    await websocket.accept()
-
-    if user_id in clients:
-        await websocket.close(code=status.WS_1008_POLICY_VIOLATION, reason="User already connected.")
-        return
-
-    clients[user_id] = websocket
-
-    if server_ws[0]:
-        await server_ws[0].send_text(await get_clients_count())
-
     try:
+        if user_id in clients:
+            old_websocket = clients[user_id]
+            await old_websocket.close(code=status.WS_1000_NORMAL_CLOSURE, reason="Another session started.")
+            await asyncio.sleep(0.1) # wait for the old connection to close
+
+        await websocket.accept()
+        clients[user_id] = websocket
+
+        if server_ws[0]:
+            await server_ws[0].send_text(await get_clients_count())
+
         while True:
+            if clients.get(user_id) != websocket:
+                break # old connection closed
+
             data = await websocket.receive_text()
+
             if data == "subscribe":
                 subscribed_clients[user_id] = websocket
             elif data == "unsubscribe":
@@ -26,7 +33,11 @@ async def websocket_client(websocket: WebSocket, user_id: str):
                 await server_ws[0].send_text(await get_clients_count())
 
     except WebSocketDisconnect:
-        clients.pop(user_id, None)
-        subscribed_clients.pop(user_id, None)
-        if server_ws[0]:
-            await server_ws[0].send_text(await get_clients_count())
+        if clients.get(user_id) == websocket:
+            clients.pop(user_id, None)
+            subscribed_clients.pop(user_id, None)
+            if server_ws[0]:
+                await server_ws[0].send_text(await get_clients_count())
+
+    except Exception as e:
+        print(f"WebSocket Error ({user_id}):", e)
